efbad025352876f8c57906ad9a8cb72f229cab4c
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-01-28 16:21:23 +0300

    Complete

--- index.js
+++ index.js
@@ -39,3 +39,26 @@ function createTimeOutEvent(employeeObject, timeOut) {
     })
     return employeeObject;
   }
+
+  function hoursWorkedOnDate(employeeObject, dateGiven) {
+    let timeIn = employeeObject.timeInEvents.find(event =>
+        event.date == dateGiven)
+    let timeOut = employeeObject.timeOutEvents.find(event =>
+        event.date == dateGiven)
+    let totalTime = (timeOut.hour - timeIn.hour) / 100
+    return totalTime;
+  }
+  function wagesEarnedOnDate(employeeObject, dateGiven) {
+    let hours = hoursWorkedOnDate(employeeObject, dateGiven)
+    return employeeObject.payPerHour * hours;
+  }
+  function allWagesFor(employeeObject) {
+    return employeeObject.timeInEvents.reduce((total, event) => {
+        return total + wagesEarnedOnDate(employeeObject, event.date)
+    }, 0)
+  }
+  function calculatePayroll(employeeRecord) {
+    return employeeRecord.reduce((total, employee) => {
+        return total + allWagesFor(employee)
+    }, 0)
+  }
